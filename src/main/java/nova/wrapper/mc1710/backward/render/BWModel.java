08a70f687cd9baa1ff4be6fae718fdc030183f46
Victorious3 <vic@nightfall.moe>
2015-04-08 13:09:31 +0200

    Re-added brightness for blocks, not sure why this was commented out.

--- src/main/java/nova/wrapper/mc1710/backward/render/BWModel.java
+++ src/main/java/nova/wrapper/mc1710/backward/render/BWModel.java
@@ -1,10 +1,12 @@
 package nova.wrapper.mc1710.backward.render;
 
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import nova.core.block.BlockAccess;
 import nova.core.render.model.Model;
+import nova.core.util.transform.Vector3i;
 import nova.wrapper.mc1710.render.RenderUtility;
 
 /**
@@ -22,40 +24,41 @@ public class BWModel extends Model {
 		tessellator.setColorRGBA_F(1, 1, 1, 1);
 
 		/**
-		 * Convert textures and UV into Minecraft equivalent. 
+		 * Convert textures and UV into Minecraft equivalent.
 		 */
 		flatten().forEach(model ->
 				model.faces.forEach(face ->
 				{
 					if (face.getBrightness() >= 0) {
-						//Brightness is defined as: skyLight << 20 | blockLight << 4
+						// Brightness is defined as: skyLight << 20 | blockLight
+						// << 4
 						tessellator.setBrightness((int) face.getBrightness());
 					} else {
-						//Determine nearest adjacent block.
-						/*Vector3i nearestPos = face.getCenter().add(face.normal.divide(2)).round();
+						// Determine nearest adjacent block.
+						Vector3i nearestPos = face.getCenter().add(face.normal.divide(2)).round();
 						Block block = blockAccess.getBlock(nearestPos.x, nearestPos.y, nearestPos.z);
 						try {
 							int brightness = block.getMixedBrightnessForBlock(blockAccess, nearestPos.x, nearestPos.y, nearestPos.z);
 
-							//TODO: Add Ambient Occlusion
-						/*
-						int aoBrightnessXYNN = block.getMixedBrightnessForBlock(blockAccess, translation.xi() - 1, translation.yi(), translation.zi());
-						int aoBrightnessYZNN = block.getMixedBrightnessForBlock(blockAccess, translation.xi(), translation.yi(), translation.zi() - 1);
-						int aoBrightnessYZNP = block.getMixedBrightnessForBlock(blockAccess, translation.xi(), translation.yi(), translation.zi() + 1);
-						int aoBrightnessXYPN = block.getMixedBrightnessForBlock(blockAccess, translation.xi() + 1, translation.yi(), translation.zi());
+							// TODO: Add Ambient Occlusion
 
-						int brightnessTopLeft = getAoBrightness(aoBrightnessXYZNNP, this.aoBrightnessXYNN, this.aoBrightnessYZNP, i1);
-						int brightnessTopRight = getAoBrightness(aoBrightnessYZNP, this.aoBrightnessXYZPNP, this.aoBrightnessXYPN, i1);
-						int brightnessBottomRight = getAoBrightness(this.aoBrightnessYZNN, this.aoBrightnessXYPN, this.aoBrightnessXYZPNN, i1);
-						int brightnessBottomLeft = getAoBrightness(this.aoBrightnessXYNN, this.aoBrightnessXYZNNN, this.aoBrightnessYZNN, i1);
-						*
+							/*int aoBrightnessXYNN = block.getMixedBrightnessForBlock(blockAccess, translation.xi() - 1, translation.yi(), translation.zi());
+							int aoBrightnessYZNN = block.getMixedBrightnessForBlock(blockAccess, translation.xi(), translation.yi(), translation.zi() - 1);
+							int aoBrightnessYZNP = block.getMixedBrightnessForBlock(blockAccess, translation.xi(), translation.yi(), translation.zi() + 1);
+							int aoBrightnessXYPN = block.getMixedBrightnessForBlock(blockAccess, translation.xi() + 1, translation.yi(), translation.zi());
+
+							int brightnessTopLeft = getAoBrightness(aoBrightnessXYZNNP, this.aoBrightnessXYNN, this.aoBrightnessYZNP, i1);
+							int brightnessTopRight = getAoBrightness(aoBrightnessYZNP, this.aoBrightnessXYZPNP, this.aoBrightnessXYPN, i1);
+							int brightnessBottomRight = getAoBrightness(this.aoBrightnessYZNN, this.aoBrightnessXYPN, this.aoBrightnessXYZPNN, i1);
+							int brightnessBottomLeft = getAoBrightness(this.aoBrightnessXYNN, this.aoBrightnessXYZNNN, this.aoBrightnessYZNN, i1);*/
+
 							tessellator.setBrightness(brightness);
 
 						} catch (Exception e) {
 							e.printStackTrace();
-						}*/
-						//TODO: Remove this
-						tessellator.setBrightness(15 << 20 | 11 << 4);
+						}
+						// TODO: Remove this
+						// tessellator.setBrightness(15 << 20 | 11 << 4);
 					}
 					tessellator.setNormal(face.normal.xf(), face.normal.yf(), face.normal.zf());
 
@@ -72,7 +75,7 @@ public class BWModel extends Model {
 						});
 					}
 				})
-		);
+			);
 	}
 
 	public void render() {
@@ -85,11 +88,12 @@ public class BWModel extends Model {
 		flatten().forEach(model ->
 				model.faces.forEach(face ->
 				{
-					//Brightness is defined as: skyLight << 20 | blockLight << 4
+					// Brightness is defined as: skyLight << 20 | blockLight <<
+					// 4
 					if (face.getBrightness() >= 0) {
 						tessellator.setBrightness((int) face.getBrightness());
 					} else {
-						//Determine nearest adjacent block.
+						// Determine nearest adjacent block.
 						tessellator.setBrightness(15 << 20 | 11 << 4);
 					}
 
@@ -108,7 +112,7 @@ public class BWModel extends Model {
 						});
 					}
 				})
-		);
+			);
 	}
 
 	public int getAoBrightness(int a, int b, int c, int d) {
